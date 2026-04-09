2ce7f554272ec6be85ebbf7e3607e67ff1cb1534
Antti Jäppinen <anttiiv@hotmail.com>
2026-04-09 22:41:38 +0300

    board audit

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "20260409130630";
+const APP_VERSION = "20260409224130";
 const STATIC_CACHE = `roborally-static-${APP_VERSION}`;
 const RUNTIME_CACHE = `roborally-runtime-${APP_VERSION}`;
 
